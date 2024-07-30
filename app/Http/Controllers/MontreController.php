ff6b4c00171094fa9059e5f01d38ed2d31d585a5
Oussama El Mabrouki <138411649+OussamaElm0@users.noreply.github.com>
2024-07-30 21:48:37 +0100

    Generate models

---
+++ app/Http/Controllers/MontreController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Montre;
+use App\Http\Requests\StoreMontreRequest;
+use App\Http\Requests\UpdateMontreRequest;
+
+class MontreController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMontreRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Montre $montre)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Montre $montre)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMontreRequest $request, Montre $montre)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Montre $montre)
+    {
+        //
+    }
+}
