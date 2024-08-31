e960e4f65b24f1219e9b065bc3a7d9e97ed2b3eb
Oussama El Mabrouki <138411649+OussamaElm0@users.noreply.github.com>
2024-08-31 14:50:38 +0100

    Display messages in the table

---
+++ app/Filament/Resources/MessageResource.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\MessageResource\Pages;
+use App\Filament\Resources\MessageResource\RelationManagers;
+use App\Models\Message;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ToggleColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class MessageResource extends Resource
+{
+    protected static ?string $model = Message::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-chat-bubble-left-right';
+
+    protected static ?int $navigationSort = 3;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('prenom')
+                    ->label('Prenom')
+                    ->searchable(),
+                TextColumn::make('nom')
+                    ->label('Nom')
+                    ->searchable(),
+                TextColumn::make('tel')
+                    ->label('Tel'),
+                TextColumn::make('message')
+                    ->label('Message')
+                    ->limit(20),
+                ToggleColumn::make('vue')
+                    ->label('Vue'),
+                TextColumn::make('created_at')
+                    ->label('Date'),
+            ])
+            ->defaultSort('created_at','desc')
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMessages::route('/'),
+            'create' => Pages\CreateMessage::route('/create'),
+            'edit' => Pages\EditMessage::route('/{record}/edit'),
+        ];
+    }
+}
