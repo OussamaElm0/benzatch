d38fcb0794a0daf0bdc98cac385a37c6e57a2673
Oussama El Mabrouki <138411649+OussamaElm0@users.noreply.github.com>
2024-09-02 18:37:17 +0100

    Hide promotions section from welcome page if there no promotion

--- resources/views/partials/welcome/promotions.blade.php
+++ resources/views/partials/welcome/promotions.blade.php
@@ -1,14 +1,17 @@
-<section class="bg-gold flex flex-col gap-4 p-4 text-l">
-    <h1 class="uppercase title place-self-center tracking-widest text-xl sm:text-2xl">
-        promotions en cours
-    </h1>
-    <div class="grid mobile:grid-cols-1 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 h-auto gap-8">
-        @foreach($montres as $montre)
-            <x-cards.montre :$montre />
-        @endforeach
-        <a href="{{ route("montres.index") }}" class="bg-white sm:h-auto flex flex-col items-center justify-center sm:col-auto gap-1 p-2 text-xl text-center break-words font-semibold hover:underline rounded">
-            Explorer Toutes Les Montres
-            <x-heroicon-o-arrow-long-right class="w-8" />
-        </a>
-    </div>
-</section>
+@if(count($montres) !== 0)
+    <section class="bg-gold flex flex-col gap-4 p-4 text-l">
+        <h1 class="uppercase title place-self-center tracking-widest text-xl sm:text-2xl">
+            promotions en cours
+        </h1>
+        <div class="grid mobile:grid-cols-1 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 h-auto gap-8">
+            @foreach($montres as $montre)
+                <x-cards.montre :$montre />
+            @endforeach
+            <a href="{{ route("montres.index") }}" class="bg-white sm:h-auto flex flex-col items-center justify-center sm:col-auto gap-1 p-2 text-xl text-center break-words font-semibold hover:underline rounded">
+                Explorer Toutes Les Montres
+                <x-heroicon-o-arrow-long-right class="w-8" />
+            </a>
+        </div>
+    </section>
+
+@endif
